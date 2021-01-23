e987e3d8b1b1965f3ed34e0bc1329d97052e3e8b
usoala <usoa.larrarte@gmail.com>
2021-01-23 02:11:32 +0000

    Lambdas

---
+++ funcionesLambda.py
@@ -0,0 +1,9 @@
+from funciones1nivel import sumaTodos 
+
+print(sumaTodos(3, lambda x: x**3))
+
+doble = lambda x: x*2
+triple = lambda x: x*3
+
+print(sumaTodos(3, doble))
+print(sumaTodos(3, triple))
