823ca4f137b08d03bb4a8c58e1085d2d40a9d334
usoala <usoa.larrarte@gmail.com>
2021-01-24 23:44:51 +0000

    Nueva tortuguita

--- objetos.py
+++ objetos.py
@@ -2,10 +2,16 @@ import turtle
 
 tortuguita = turtle.Turtle()
 otraTortuguita = turtle.Turtle()
+lentorra = turtle.Turtle()
 
 tortuguita.shape("turtle")
+tortuguita.color("blue")
 tortuguita.fd(50)
+tortuguita.speed(5)
 
 otraTortuguita.color("green")
 otraTortuguita.left(90)
 otraTortuguita.fd(50)
+otraTortuguita.speed(5)
+
+lentorra.speed(1)
